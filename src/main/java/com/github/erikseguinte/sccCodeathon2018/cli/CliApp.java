ee95f322bbc2d5bd653cad397ab03a71c1ee4a9f
AST <ErikSeguinte@me.com>
2018-04-12 14:04:04 -0700

    Loads files using classloader

---
+++ src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/CliApp.java
@@ -0,0 +1,20 @@
+package com.github.erikseguinte.sccCodeathon2018.cli;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class CliApp {
+
+    public static void main(String[] args) {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current relative path is: " + s);
+        ArrayList<String> classes = FileReader.getClasses("text/Students/w1234567.txt");
+
+        for (String thisClass:classes) {
+            System.out.println(thisClass);
+        }
+
+    }
+}
---
+++ src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/FileReader.java
@@ -0,0 +1,28 @@
+package com.github.erikseguinte.sccCodeathon2018.cli;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileReader {
+
+    public static ArrayList<String> getClasses(String filename){
+
+        ArrayList<String> classes = new ArrayList<>();
+        ClassLoader classLoader = FileReader.class.getClassLoader();
+
+        // Try-with-resources
+        try(Scanner scanner = new Scanner(classLoader.getResourceAsStream(filename))) {
+
+            while(scanner.hasNextLine()){
+
+               classes.add(scanner.nextLine());
+            }
+        }
+
+        return classes;
+    }
+}
