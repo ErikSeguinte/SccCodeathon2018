b1a84e4d37a6ab80edb2d0ede66e8afa80b4cee6
AST <ErikSeguinte@me.com>
2018-04-12 14:17:29 -0700

    refactors cleanup to its own method

--- src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/CliApp.java
+++ src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/CliApp.java
@@ -10,7 +10,7 @@ public class CliApp {
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
         System.out.println("Current relative path is: " + s);
-        ArrayList<String> classes = FileReader.getClasses("text/goals/ascs.txt");
+        ArrayList<String> classes = FileReader.getClassesFromResource("text/goals/ascs.txt");
 
         for (String thisClass:classes) {
             System.out.println(thisClass);
--- src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/FileReader.java
+++ src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/FileReader.java
@@ -1,15 +1,11 @@
 package com.github.erikseguinte.sccCodeathon2018.cli;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileReader {
 
-    public static ArrayList<String> getClasses(String filename){
+    public static ArrayList<String> getClassesFromResource(String filename){
 
         ArrayList<String> classes = new ArrayList<>();
         ClassLoader classLoader = FileReader.class.getClassLoader();
@@ -18,20 +14,27 @@ public class FileReader {
         try(Scanner scanner = new Scanner(classLoader.getResourceAsStream(filename))) {
 
             while(scanner.hasNextLine()){
+                String thisClass = scanner.nextLine();
 
-                String thisClass = scanner.nextLine().trim();
-
-                //lower Case
-                thisClass = thisClass.toLowerCase();
-
-                // remove internal spaces
-                thisClass = thisClass.replaceAll("\\s","");
-
+                thisClass = cleanUpString(thisClass);
 
                 classes.add(thisClass);
             }
         }
 
         return classes;
+    }
+
+    private static String cleanUpString(String string){
+
+        string = string.trim();
+
+        //lower Case
+        string = string.toLowerCase();
+
+        // remove internal spaces
+        string = string.replaceAll("\\s","");
+
+        return string;
     }
 }
