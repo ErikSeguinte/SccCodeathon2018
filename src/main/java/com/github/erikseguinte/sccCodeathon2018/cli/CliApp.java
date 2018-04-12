19c523380ae5bdbceb79360e92b305ee39438719
AST <ErikSeguinte@me.com>
2018-04-12 14:56:46 -0700

    Can compare goal to classes taken;

--- src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/CliApp.java
+++ src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/CliApp.java
@@ -10,8 +10,10 @@ public class CliApp {
     public static void main(String[] args) {
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
-        System.out.println("Current relative path is: " + s);
-        HashSet<String> classes = FileReader.getClassesFromResource("text/goals/ascs.txt");
+        HashSet<String> classesTaken = FileReader.getClassesFromResource("text/Students/w1234567.txt");
+        Goal goal = new Goal(FileReader.getClassesFromResource("text/Students/w1235813.txt"));
+
+        HashSet<String> classes = goal.compareToRequirements(classesTaken);
 
         for (String thisClass:classes) {
             System.out.println(thisClass);
--- src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/Goal.java
+++ src/main/java/com/github/erikseguinte/sccCodeathon2018/cli/Goal.java
@@ -7,9 +7,20 @@ public class Goal {
     private HashSet<String> requiredClasses;
     private HashSet<String> electives;
 
+    Goal(HashSet<String> requiredClasses) {
+        this.requiredClasses = requiredClasses;
+    }
+
     public HashSet<String> compareToRequirements(HashSet<String> classesTaken){
 
         HashSet<String> classesRemaining = new HashSet<>();
+
+
+        for (String thisClass: requiredClasses){
+            if (!classesTaken.contains(thisClass)){
+                classesRemaining.add(thisClass);
+            }
+        }
         return classesRemaining;
 
     }
