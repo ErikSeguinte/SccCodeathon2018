6c4c991ce296907f0b801040aa8592f753bc54ae
Erik Seguinte <ErikSeguinte@gmail.com>
2018-04-13 14:07:33 -0700

    work donw on student

--- src/main/java/com/github/erikseguinte/sccCodeathon2018/backend/Student.java
+++ src/main/java/com/github/erikseguinte/sccCodeathon2018/backend/Student.java
@@ -1,4 +1,41 @@
 package com.github.erikseguinte.sccCodeathon2018.backend;
 
+import java.util.EnumSet;
+import java.util.HashSet;
+
+enum OopClasses{
+    CISP400, CISP401
+};
+
 public class Student {
+    private HashSet<String> classesTaken;
+    private Goal goal;
+    private int electiveUnits;
+    private EnumSet<OopClasses> oop;
+
+    public void setClassesTaken(HashSet<String> classesTaken) {
+        this.classesTaken = classesTaken;
+
+        oop = EnumSet.noneOf(OopClasses.class);
+
+        if (classesTaken.contains("cisp400")){
+            oop.add(OopClasses.CISP400);
+        }
+
+        if (classesTaken.contains("cisp401")){
+            oop.add(OopClasses.CISP401);
+        }
+    }
+
+    public EnumSet<OopClasses> getOop() {
+        return oop;
+    }
+
+    public HashSet<String> getClassesTaken() {
+        return classesTaken;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
 }
